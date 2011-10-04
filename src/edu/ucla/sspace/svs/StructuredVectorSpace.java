08f15cd6512087fb0173517b9baf878054257822
fozziethebeat <fozziethebeat@gmail.com>
2011-10-03 20:04:20 -0700

    Refactoring SVS

--- src/edu/ucla/sspace/svs/StructuredVectorSpace.java
+++ src/edu/ucla/sspace/svs/StructuredVectorSpace.java
@@ -340,7 +340,7 @@ public class StructuredVectorSpace implements SemanticSpace, Serializable {
                 synchronized (this) {
                     preference = preferenceVectors.get(p.x);
                     if (preference == null) {
-                        preference = new SelectionalPreference();
+                        preference = new SelectionalPreference(combinor);
                         preferenceVectors.put(p.x, preference);
                     }
                 }
@@ -450,76 +450,5 @@ public class StructuredVectorSpace implements SemanticSpace, Serializable {
     private boolean acceptWord(String word) {
         return !word.equals(EMPTY_STRING) && 
                (semanticFilter.isEmpty() || semanticFilter.contains(word));
-    }
-
-    private class RelationTuple {
-        public int head;
-        public String relation;
-
-        public RelationTuple(int head, String relation) {
-            this.head = head;
-            this.relation = relation;
-        }
-
-        public boolean equals(Object o) {
-            if (o == null || !(o instanceof RelationTuple))
-                return false;
-            RelationTuple r = (RelationTuple) o;
-            return this.head == r.head && this.relation == r.relation;
-        }
-
-        public int hashCode() {
-            return head ^ relation.hashCode();
-        }
-    }
-
-    private class SelectionalPreference implements Serializable {
-
-        private static final long serialVersionUID = 1L;
-
-        public SparseDoubleVector lemmaVector;
-        public Map<String, SparseDoubleVector> selPreferences;
-        public Map<String, SparseDoubleVector> inverseSelPreferences;
-
-        public SelectionalPreference() {
-            lemmaVector = new CompactSparseVector();
-            selPreferences = new HashMap<String, SparseDoubleVector>();
-            inverseSelPreferences = new HashMap<String, SparseDoubleVector>();
-        }
-
-        public void addPreference(String relation,
-                                  SparseDoubleVector vector,
-                                  double frequency) {
-            add(relation, new ScaledSparseDoubleVector(vector, frequency),
-                selPreferences);
-        }
-
-        public void addInversePreference(String relation, 
-                                         SparseDoubleVector vector,
-                                         double frequency) {
-            add(relation, new ScaledSparseDoubleVector(vector, frequency),
-                inverseSelPreferences);
-        }
-
-        private void add(String relation, 
-                         SparseDoubleVector vector,
-                         Map<String, SparseDoubleVector> map) {
-            SparseDoubleVector preference = map.get(relation);
-            if (preference == null) {
-                map.put(relation,
-                        (SparseDoubleVector) Vectors.copyOf(vector));
-                return;
-            }
-
-            map.put(relation, combinor.combine(preference, vector));
-        }
-
-        public SparseDoubleVector preference(String relation) {
-            return selPreferences.get(relation);
-        }
-
-        public SparseDoubleVector inversePreference(String relation) {
-            return inverseSelPreferences.get(relation);
-        }
     }
 }
