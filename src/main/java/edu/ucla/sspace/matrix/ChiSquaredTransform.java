1efbd5f423d748818b8031880709fb0468831223
Keith Stevens <keith@kracken.(none)>
2012-04-23 13:09:16 -0700

    Retrofitting the existing global transform implementations to use the Signifigance Test implmenetations

--- src/main/java/edu/ucla/sspace/matrix/ChiSquaredTransform.java
+++ src/main/java/edu/ucla/sspace/matrix/ChiSquaredTransform.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011, Lawrence Livermore National Security, LLC. Produced at
+ * Copyright (c) 2012, Lawrence Livermore National Security, LLC. Produced at
  * the Lawrence Livermore National Laboratory. Written by Keith Stevens,
  * kstevens@cs.ucla.edu OCEC-10-073 All rights reserved. 
  *
@@ -23,135 +23,17 @@
 
 package edu.ucla.sspace.matrix;
 
-import edu.ucla.sspace.matrix.MatrixIO.Format;
-import edu.ucla.sspace.matrix.TransformStatistics.MatrixStatistics;
-
-import java.io.File;
+import edu.ucla.sspace.common.statistics.ChiSquaredTest;
 
 
 /**
  * Computes the <a
  * href="http://en.wikipedia.org/wiki/Pearson%27s_chi-squared_test">Pearson
  * Chi-Squared</a> value for each joint event stored in a contingency matrix.
- *
  * @author Keith Stevens
  */
-public class ChiSquaredTransform extends BaseTransform {
-
-    /**
-     * {@inheritDoc}
-     */
-    protected GlobalTransform getTransform(Matrix matrix) {
-        return new ChiSquaredGlobalTransform(matrix);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    protected GlobalTransform getTransform(File inputMatrixFile,
-                                           MatrixIO.Format format) {
-        return new ChiSquaredGlobalTransform(inputMatrixFile, format);
-    }
-
-    /**
-     * Returns the name of this transform.
-     */
-    public String toString() {
-        return "ChiSquared";
-    }
-
-    public class ChiSquaredGlobalTransform implements GlobalTransform {
-
-        /**
-         * The total sum of occurances for each row (row).
-         */
-        private double[] rowSums;
-
-        /**
-         * The total sum of occurances for each col (column).
-         */
-        private double[] columnSums;
-
-        /**
-         * The total summation of the entire matrix.
-         */
-        private double total;
-
-        /**
-         * Creates an instance of {@code ChiSquaredTransform}
-         * from a given {@link Matrix}.
-         */
-        public ChiSquaredGlobalTransform(Matrix matrix) {
-            MatrixStatistics stats =
-                TransformStatistics.extractStatistics(matrix);
-            rowSums = stats.rowSums;
-            columnSums = stats.columnSums;
-            total = stats.matrixSum;
-        }
-
-        /**
-         * Creates an instance of {@code ChiSquaredTransform}
-         * from a matrix {@code File} of format {@code format}.
-         */
-        public ChiSquaredGlobalTransform(
-                File inputMatrixFile,
-                MatrixIO.Format format) {
-            MatrixStatistics stats =
-                TransformStatistics.extractStatistics(inputMatrixFile, format);
-            rowSums = stats.rowSums;
-            columnSums = stats.columnSums;
-            total = stats.matrixSum;
-        }
-
-        /**
-         * Computes the Pearson Chi-Squared value for the joint event defined by
-         * {@code row} and {@code col} with {@code value} specifying the number
-         * of occurances of {@code row} with {@code col}.   
-         *
-         * @param row The index specifying the first event being observed
-         * @param col The index specifying the second event being observed
-         * @param value The number of joint occurrences
-         */
-        public double transform(int row, int col, double both) {
-            double justA = rowSums[row] - both;
-            double justB = columnSums[col] - both;
-            double neither = total - justA - justB - both;
-            return score(both, justA, justB, neither);
-        }
-    }
-
-    /**
-     * Returns the Pearson Chi-Squared test of significance using one degree of
-     * freedom.  To identify events with a significance value above .95, or p
-     * &ample; .05, reject any chi-squared values less than 3.841.
-     */
-    public static double score(double both, 
-                               double justA, 
-                               double justB, 
-                               double neither) {
-        // Think of the table as
-        //      B       !B
-        //   A: both    justA   : row1Sum
-        //  !A: justB   neither : row2Sum
-        //  ---------------------------
-        //      col1Sum col2Sum : sum
-        double col1sum = both + justB; 
-        double col2sum = justA + neither;
-        double row1sum = both + justA; 
-        double row2sum = justB + neither;
-        double sum = row1sum + row2sum;
-        
-        // Calculate the expected values for a, b, c, d
-        // The expected value is coliSum * rowjSum / sum 
-        double aExp = (row1sum / sum) * col1sum;
-        double bExp = (row1sum / sum) * col2sum;
-        double cExp = (row2sum / sum) * col1sum;
-        double dExp = (row2sum / sum) * col2sum;
-
-        // Chi-squared is (Observed - Expected)^2 / Expected
-        return (Math.pow(both-aExp, 2) / aExp) +
-               (Math.pow(justA-bExp, 2) / bExp) +
-               (Math.pow(justB-cExp, 2) / cExp) +
-               (Math.pow(neither-dExp, 2) / dExp);
+public class ChiSquaredTransform extends SignificanceMatrixTransform {
+    public ChiSquaredTransform() {
+        super(new ChiSquaredTest());
     }
 }
