06875ecc0c442b1deaf95ccfe0eb25448372b9f0
fozziethebeat <fozziethebeat@gmail.com>
2012-06-04 15:02:30 -0700

    Adding some handy vector io methods

--- src/test/java/edu/ucla/sspace/vector/VectorIOTest.java
+++ src/test/java/edu/ucla/sspace/vector/VectorIOTest.java
@@ -21,6 +21,14 @@
 
 package edu.ucla.sspace.vector;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.StringReader;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Ignore;
 import org.junit.Test;
 
@@ -31,6 +39,81 @@ import static org.junit.Assert.*;
  * @author Keith Stevens
  */
 public class VectorIOTest {
+
+    @Test public void testReadSparseVector() throws IOException {
+        String testInput = "4 10\n" +
+                           "0:21.4 9:123.4\n" +
+                           "1:0.4 8:1E-4\n" +
+                           "3:1.0 4:2.0 5:3.4\n" +
+                           "5:2.0 6:1.4\n";
+        List<SparseDoubleVector> output = VectorIO.readSparseVectors(
+                new StringReader(testInput));
+        assertEquals(4, output.size());
+
+        SparseDoubleVector v1 = output.get(0);
+        assertEquals(10, v1.length());
+        assertEquals(2, v1.getNonZeroIndices().length);
+        assertEquals(21.4, v1.get(0), 0.0000000001);
+        assertEquals(123.4, v1.get(9), 0.0000000001);
+
+        SparseDoubleVector v2 = output.get(1);
+        assertEquals(10, v2.length());
+        assertEquals(2, v2.getNonZeroIndices().length);
+        assertEquals(0.4, v2.get(1), 0.0000000001);
+        assertEquals(1E-4, v2.get(8), 0.0000000001);
+
+        SparseDoubleVector v3 = output.get(2);
+        assertEquals(10, v3.length());
+        assertEquals(3, v3.getNonZeroIndices().length);
+        assertEquals(1.0, v3.get(3), 0.0000000001);
+        assertEquals(2.0, v3.get(4), 0.0000000001);
+        assertEquals(3.4, v3.get(5), 0.0000000001);
+
+        SparseDoubleVector v4 = output.get(3);
+        assertEquals(10, v4.length());
+        assertEquals(2, v4.getNonZeroIndices().length);
+        assertEquals(2.0, v4.get(5), 0.0000000001);
+        assertEquals(1.4, v4.get(6), 0.0000000001);
+    }
+
+    @Test public void testWriteSparseVectors() {
+        List<SparseDoubleVector> vectors =
+            new ArrayList<SparseDoubleVector>();
+
+        SparseDoubleVector v = new CompactSparseVector(4);
+        v.set(0, 1.0);
+        v.set(3, 1.9);
+        vectors.add(v);
+
+        v = new CompactSparseVector(4);
+        v.set(2, 1.4);
+        v.set(1, 0.9);
+        vectors.add(v);
+
+        v = new CompactSparseVector(4);
+        v.set(0, 1.4);
+        v.set(1, 1.4);
+        v.set(2, 1.4);
+        v.set(3, 0.9);
+        vectors.add(v);
+
+        ByteArrayOutputStream resultStream = 
+            new ByteArrayOutputStream();
+        PrintStream ps = new PrintStream(resultStream);
+        VectorIO.writeVectors(vectors, ps);
+        ps.close();
+
+        String result = resultStream.toString();
+        String[] lines = result.split("\\n");
+        assertEquals("3 4", lines[0]);
+        for (int i = 1; i < lines.length; ++i) 
+            for (String line : lines[i].split("\\s+")) {
+                String[] parts = line.split(":");
+                int col = Integer.parseInt(parts[0]);
+                double val = Double.parseDouble(parts[1]);
+                assertEquals(vectors.get(i-1).get(col), val, 0.0000001);
+            }
+    }
 
     @Test public void testSparseToString() {
         Vector vector = new CompactSparseVector(new double[]{0, 0, 0, 1});
