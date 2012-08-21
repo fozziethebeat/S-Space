c735cd57a4491d099e76dda282de4c6e27f8ef7b
fozziethebeat <fozziethebeat@gmail.com>
2012-08-21 11:58:53 +0900

    Moving expiermental directories to another separate repository to keep things clean and distanced

--- TwitterEval/src/main/scala/edu/ucla/sspace/ClassifierData.scala
+++
@@ -1,4 +0,0 @@
-package edu.ucla.sspace
-
-case class ClassifierData(numCategories: Int, priorTokenCounts: Double, priorCategoryCounts: Array[Double], categories: Array[Category])
-case class Category(instanceCounts: Double, tokenCounts: Map[String, Double])
